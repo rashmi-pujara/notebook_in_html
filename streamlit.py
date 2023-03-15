d1906b37e801579e988c20994f29c984add2ff61
rashmi-pujara <rashmi.pujara1@gmail.com>
2023-03-14 20:01:45 -0400

    streamlit

--- streamlit.py
+++ streamlit.py
@@ -1,14 +1,45 @@
 import streamlit as st
 from transformers import pipeline
+from PIL import Image
+import io
 
-model_checkpoint = "Modfiededition/t5-base-fine-tuned-on-jfleg"
-model = pipeline("text2text-generation", model=model_checkpoint)
+from transformers import MobileViTFeatureExtractor, MobileViTForImageClassification
+
+model_checkpoint = "valurank/distilroberta-news-small"
+model = pipeline("text-classification", model=model_checkpoint)
+
+def load_image():
+    uploaded_file = st.file_uploader(label='Pick an image to test')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+    
+def load_model():
+    model = MobileViTForImageClassification.from_pretrained("apple/mobilevit-small")
+    return model
+    
+def load_feature_extractor():
+    feature_extractor = MobileViTFeatureExtractor.from_pretrained("apple/mobilevit-small")
+    return feature_extractor
 
 def main():
-    st.title("Grammar Correction")
-    input = st.text_input("Enter the sentence")
-    output = model(input)
-    st.write(output[0]['generated_text'])
-    
-if __name__ == '__main__':
+    st.title('Image Classification')
+    image = load_image()
+    print(image)
+    model = load_model()
+    if image is not None:
+        feature_extractor = load_feature_extractor()
+        
+        inputs = feature_extractor(images=image, return_tensors="pt")
+        outputs = model(**inputs)
+        logits = outputs.logits
+        
+        # model predicts one of the 1000 ImageNet classes
+        predicted_class_idx = logits.argmax(-1).item()
+        st.metric("Predicted class:", model.config.id2label[predicted_class_idx])
+
+if __name__ == "_main_":
     main()
